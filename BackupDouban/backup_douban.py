041f8dd964d228cdde0a5a78fd0d0c4c126e888c
Kyra <kyrayang.5@gmail.com>
2020-10-23 14:07:44 +0800

    run scrapy through script

---
+++ BackupDouban/backup_douban.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from scrapy.crawler import CrawlerProcess
+import click
+
+@click.command()
+@clicke.argument("account_name")
+def main(account_name):
+    
+    
+
+if __name__ == "__main__":
+    main()
